c5bd0ef6586ef7f085154f6d11028f7f17e171b2
Richard Hull <rm_hull@yahoo.co.uk>
2012-11-16 22:34:01 +0000

    Compile error, documentation & converted to threading

--- src/euler025.clj
+++ src/euler025.clj
@@ -2,24 +2,24 @@
 ;; ==========
 ;; The Fibonacci sequence is defined by the recurrence relation:
 ;;
-;;    Fn = Fn1 + Fn2, where F1 = 1 and F2 = 1.
+;;    F[n] = F[n-1] + F[n-2], where F[1] = 1 and F[2] = 1.
 ;;
 ;; Hence the first 12 terms will be:
 ;;
-;;    F1 = 1
-;;    F2 = 1
-;;    F3 = 2
-;;    F4 = 3
-;;    F5 = 5
-;;    F6 = 8
-;;    F7 = 13
-;;    F8 = 21
-;;    F9 = 34
-;;    F10 = 55
-;;    F11 = 89
-;;    F12 = 144
+;;    F[1] = 1
+;;    F[2] = 1
+;;    F[3] = 2
+;;    F[4] = 3
+;;    F[5] = 5
+;;    F[6] = 8
+;;    F[7] = 13
+;;    F[8] = 21
+;;    F[9] = 34
+;;    F[10] = 55
+;;    F[11] = 89
+;;    F[12] = 144
 ;;
-;; The 12th term, F12, is the first term to contain three digits.
+;; The 12th term, F[12], is the first term to contain three digits.
 ;;
 ;; What is the first term in the Fibonacci sequence to contain 1000 digits?
 
@@ -29,8 +29,8 @@
 
 (defn solve [n]
   (let [limit (iexpt (bigint 10) (dec n))]
-    (first 
-      (drop-while #(< (second %) limit) 
-                  (map vector integers fib-seq)))))
+    (->> (map vector integers fibonacci-seq)
+         (drop-while #(< (second %) limit))
+         first))) 
 
 (time (solve 1000))
