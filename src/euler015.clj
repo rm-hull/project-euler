4f843aa326cb6474d02dfbfad03be495283eaf94
Richard Hull <rm_hull@yahoo.co.uk>
2012-11-16 22:01:55 +0000

    Comments

--- src/euler015.clj
+++ src/euler015.clj
@@ -3,7 +3,7 @@
 ;; Starting in the top left corner of a 2x2 grid, there are 6 routes 
 ;; (without backtracking) to the bottom right corner.
 ;;
-;; How many routes are there through a 2020 grid?
+;; How many routes are there through a 20x20 grid?
 ;;
 ;; 1 = Down / 0 = Across
 ;;
