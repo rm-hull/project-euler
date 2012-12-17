2987899751cd78785003e85f7c41fcfa485fe341
Richard Hull <rm_hull@yahoo.co.uk>
2012-12-17 23:05:09 +0000

    Solution for #104

--- src/euler104.clj
+++ src/euler104.clj
@@ -14,14 +14,45 @@
 ;; digits AND the last nine digits are 1-9 pandigital, find k.
 ;;
 
-(ns euler104
-  (:use [util.fibonacci]
-        [util.misc]
-        [util.primes]))
+(ns euler104)
 
-(bigint (+ (/ (iexpt (bigdec golden-ratio) 541) (Math/sqrt 5)) 1/2))
+(defn tail-limiting-fib-seq [modulus]
+  (letfn [(limit [n] (if (> n modulus) (rem n modulus) n))
+          (seq0 [a b] (lazy-seq (cons a (seq0 b (limit (+ a b))))))]
+    (seq0 1 1)))
 
-(nth fib-seq 540)
-(nth fib-seq 2748)
+(defn pandigital? [^long n]
+  (if (< n 123456789)
+    false
+    (loop [n      n
+           digits 0]
+      (if (zero? n)
+        (= digits 1022)
+        (recur 
+          (quot n 10)
+          (bit-or 
+            digits
+            (bit-shift-left 1 (- n (* (quot n 10) 10)))))))))
 
-(prime-factors-of 542)
+; Binet's formula for working out n'th Fibonacci number
+;   F[n] = phi^n/sqrt(5)
+;
+; log10(F[n]) = log10(phi^n/sqrt(5))
+; log10(F[n]) = n * log10(phi) - log10(sqrt(5))
+
+(def log10-sqrt5 (Math/log10 (Math/sqrt 5))) 
+
+(def log10-phi (Math/log10 golden-ratio))
+
+(defn head-limiting-fib [^long n] 
+  (let [t (- (* n log10-phi) log10-sqrt5)]
+    (long (Math/pow 10 (- t (long t) -8))))) ; limit to most significant 9 digits
+
+(defn solve []
+  (->> 
+    (map vector integers (tail-limiting-fib-seq 1000000000))
+    (filter #(pandigital? (second %)))
+    (filter #(pandigital? (head-limiting-fib (first %)))) 
+    first))
+
+(time (solve))
