736170e44a733640a62cf85724b1ee4d609d4408
Richard Hull <rm_hull@yahoo.co.uk>
2012-12-21 22:48:32 +0000

    still not truckin'

--- src/euler088.clj
+++ src/euler088.clj
@@ -1,100 +1,148 @@
 ;; EULER #088
 ;; ==========
 ;; A natural number, N, that can be written as the sum and product of a 
-;; given set of at least two natural numbers, {a1, a2, ... , ak} is called 
-;; a product-sum number: N = a1 + a2 + ... + ak = a1 x a2 x ... x ak.
+;; given set of at least two natural numbers, {a[1], a[2], ... , a[k]} is
+;; called a product-sum number: N = a[1] + a[2] + ... + a[k] = 
+;; a[1] x a[2] x ... x a[k].
 ;;
 ;; For example, 6 = 1 + 2 + 3 = 1 x 2 x 3.
 ;;
-;; For a given set of size, k, we shall call the smallest N with this 
-;; property a minimal product-sum number. The minimal product-sum numbers 
+;; For a given set of size, k, we shall call the smallest N with this
+;; property a minimal product-sum number. The minimal product-sum numbers
 ;; for sets of size, k = 2, 3, 4, 5, and 6 are as follows.
 ;;
-;;    k=2: 4  = 2 x 2                 = 2 + 2
-;;    k=3: 6  = 1 x 2 x 3             = 1 + 2 + 3
-;;    k=4: 8  = 1 x 1 x 2 x 4         = 1 + 1 + 2 + 4
-;;    k=5: 8  = 1 x 1 x 2 x 2 x 2     = 1 + 1 + 2 + 2 + 2
+;;    k=2: 4 = 2 x 2 = 2 + 2
+;;    k=3: 6 = 1 x 2 x 3 = 1 + 2 + 3
+;;    k=4: 8 = 1 x 1 x 2 x 4 = 1 + 1 + 2 + 4
+;;    k=5: 8 = 1 x 1 x 2 x 2 x 2 = 1 + 1 + 2 + 2 + 2
 ;;    k=6: 12 = 1 x 1 x 1 x 1 x 2 x 6 = 1 + 1 + 1 + 1 + 2 + 6
 ;;
-;; Hence for 2 <= k <= 6, the sum of all the minimal product-sum numbers is
-;; 4 + 6 + 8 + 12 = 30; note that 8 is only counted once in the sum.
-;; 
+;; Hence for 2<=k<=6, the sum of all the minimal product-sum numbers is 
+;; 4+6+8+12 = 30; note that 8 is only counted once in the sum.
+;;
 ;; In fact, as the complete set of minimal product-sum numbers for 
-;; 2 <= k <= 12 is {4, 6, 8, 12, 15, 16}, the sum is 61.
+;; 2<=k<=12 is {4, 6, 8, 12, 15, 16}, the sum is 61.
 ;;
-;; What is the sum of all the minimal product-sum numbers for 
-;; 2 <= k <= 12000?
+;; What is the sum of all the minimal product-sum numbers for 2<=k<=12000?
 ;;
 
-;            +  * 
-; 11122   =  7  4
-; 11123   =  8  6
-; 11124   =  9  8
-; 11125   = 10 10
-
-; 11222   =  8  8
-; 12222   =  9 16
-
-; 221111  =  8  4
-; 321111  =  9  6
-; 421111  = 10  8
-; 521111  = 11 10
-; 621111  = 12 12
-
-; 222111  =  9  8
-; 322111  = 10 12
-
-; 2211111 =  9  4
-; 3211111 = 10  6
-; 4211111 = 11  8
-; 5211111 = 12 10
-; 6211111 = 13 12
-; 7211111 = 14 14
-
-; 2221111 = 10  6
-; 3221111 = 11 12
-
- 
 (ns euler088
-  (:use [util.primes]
-        [util.combinatorics]
-        [util.misc]))
+  (:use [util.combinatorics]))
 
-(prime-factors-of 60)
-(prime-factors-of 60)
+(defn fill [xs k]
+  (let [size (count xs)]
+    (if (>= size k)
+      xs
+      (concat (repeat (- k size) 1) xs))))
 
-(reductions * (prime-factors-of 60))
+(defn expand [k n]
+  (mapcat #(repeat n %) (range 2 (inc k))))
 
-(divisors 12)
+(defn product-sum [xs]
+  { :set xs :product (reduce * xs) :sum (reduce + xs) })
 
-(prime-factors-of 8)
+(defn product-sum? [m]
+  (= (reduce * m) (reduce + m)))
 
-(->> (divisors 12)
-     (filter #(> % 1))
-     (filter #(< % 12))
-     (map #(quot 12 %)))
+(defn in-range? [lower upper xs]
+  (let [res (reduce * xs)]
+    (and 
+      (>= res lower)
+      (<= res upper))))
 
-1,2,3,4
+(defn all-combinations [k]
+  (let [ks (range 1 (inc k))]
+    (->>
+      (mapcat #(combinations (expand k %) %) ks)
+      (map #(fill % k))
+      (filter (partial in-range? k (* k 2)))
+      distinct)))
+
+(->>
+  (all-combinations 8)
+  reverse
+  (filter product-sum?)
+  first
+  (apply +)
+)
+
+(expand 6 3)
+
+(distinct (combinations (list 2 2 2 3 3 3 4 4 4 5 5 5 6 6 6) 3))
+
+(distinct (map #(fill % 6) (combinations (expand 6 2) 2)))
+
+(distinct (map #(fill % 6) (combinations (expand 6 3) 3)))
+
+(distinct (map #(fill % 6) (combinations (expand 6 4) 4)))
+
+(distinct (map #(fill % 6) (combinations (expand 6 6) 6)))
+
+(fill (list 2 2 2) 6)
+
+(fill (list 2 2 2) 6)
+
+(expand 6 3)
+
+(all-combinations 5)
 
 
-; 60 = 2 x 30
-;    = 3 x 20
-;    = 5 x 12
 
-; 30 = 2 x 15
-;    = 3 x 10
-;    = 5 x 6
+(def partitions 
+  (memoize
+    (fn [n limit]
+      (if (zero? n)
+        [[]]
+        (for [x (range (min n limit) 0 -1)
+              p (partitions (- n x) x)]
+          (cons x p))))))
 
-; 12 = 2 x 6
-;    = 2 x 2 x 3 
-;    = 3 x 4
-;    = 3 x 2 x 2
 
-; 6  = 2 x 3
-;    = 3 x 2
+(defn partitions 
+  ([n] (partitions n n)) 
+  ([n limit]
+    (if (zero? n)
+      [[]]
+      (for [x (range (min n limit) 0 -1)
+            p (partitions (- n x) x)]
+        (cons x p)))))
 
-(for [x '(2 2 3)
-      y '(2 2 3)]
-  [x y])
 
-(combinations '(2 3 5) 3)
+
+(defn product-sum [n]
+  (->> 
+    (partitions n n)
+    rest
+    (filter #(= n (reduce * %)))
+    (map #(vector (count %) %))
+    (into {})
+    ))
+
+
+(->>
+  (range 4 100 2)
+  (map product-sum)
+  )
+
+(product-sum 4)
+(product-sum 6)
+(product-sum 8)
+(product-sum 10)
+(product-sum 12)
+(product-sum 14)
+(product-sum 16)
+(product-sum 18)
+(product-sum 20)
+(product-sum 22)
+(product-sum 24)
+(product-sum 26)
+(product-sum 28)
+(product-sum 30)
+(product-sum 32)
+(product-sum 34)
+
+
+
+(partitions 5 4)
+
+(permutations '(2 2 1))
